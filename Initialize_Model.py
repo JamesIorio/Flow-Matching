8ccdf5cfbfc0f4697add6ece27d41715ff7e95a8
James Iorio <jiorio6@gatech.edu>
2026-01-06 11:58:48 -0500

    Initialize model and optimizer in Initialize_Model.py
    Added model initialization and optimizer setup.

--- Initialize_Model.py
+++ Initialize_Model.py
@@ -1,3 +1,4 @@
+# Initalization of Model
 model = MLP(img_size=28, hidden=512, num_classes=10, class_cond=True).to(device)
 optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-4)
 scaler = torch.amp.GradScaler()
